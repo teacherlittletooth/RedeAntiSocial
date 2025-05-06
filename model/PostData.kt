097cffde061c1cd8b44bcbfc045bdd03b9298ec4
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2025-05-06 14:01:32 -0300

    Update PostData.kt

--- model/PostData.kt
+++ model/PostData.kt
@@ -1,11 +1,11 @@
-package br.qi.socialmedianoite.model
+package br.qi.socialmediatarde.model
 
-import br.qi.socialmedianoite.R
+import br.qi.socialmediatarde.R
 
 data class PostData(
     var imageProfile: Int = R.drawable.ic_launcher_foreground,
     var nameProfile: String = "Nome do perfil",
     var timeProfile: String = "Agora",
     var imagePost: Int = R.drawable.ic_launcher_background,
-    var textPost: String = "Texto da postagem"
+    var textPost: String = "Texto da postagem."
 )
