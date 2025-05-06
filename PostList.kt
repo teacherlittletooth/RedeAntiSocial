e7ac21e52cb9adb7c54a40e5256cd25ee6c5aa9a
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2025-05-06 14:01:12 -0300

    Update PostList.kt

--- PostList.kt
+++ PostList.kt
@@ -1,48 +1,52 @@
-package br.qi.socialmedianoite
+package br.qi.socialmediatarde
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.qi.socialmedianoite.model.PostData
-import br.qi.socialmedianoite.ui.theme.SocialMediaNoiteTheme
+import br.qi.socialmediatarde.model.PostData
+import br.qi.socialmediatarde.ui.theme.SocialMediaTardeTheme
 
 @Composable
 fun ListPost(
-    list: List<PostData> = List(10) { PostData(nameProfile = "Usuário $it") },
+    list: List<PostData> = List(10) { PostData() },
     modifier: Modifier = Modifier
 ) {
-    LazyColumn {
+    LazyColumn(
+        state = LazyListState()
+    ) {
         items(list) {
             Surface(
-                modifier = modifier.padding(10.dp)
+                modifier = modifier.padding(8.dp)
             ) {
                 PostPage(
                     imageProfile = it.imageProfile,
                     nameProfile = it.nameProfile,
                     timeProfile = it.timeProfile,
                     imagePost = it.imagePost,
-                    textPost = it.textPost
+                    textPost = it.textPost,
                 )
             }
         }
     }
 }
 
-///////////////////////////////////////////
+///////////////////////////////////////////////
 @Preview(
     showBackground = true,
     heightDp = 400
 )
 @Composable
 fun ListPostPreview() {
-    SocialMediaNoiteTheme {
+    SocialMediaTardeTheme {
         ListPost()
     }
 }
