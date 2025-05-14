bc3cbed5c6414a16a7efee14477e309fea6001fe
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2025-05-14 15:18:23 -0300

    Update PostData.kt

--- model/PostData.kt
+++ model/PostData.kt
@@ -1,11 +1,11 @@
-package br.qi.socialmediatarde.model
+package br.com.redeantisocialtarde.model
 
-import br.qi.socialmediatarde.R
+import br.com.redeantisocialtarde.R
 
 data class PostData(
-    var imageProfile: Int = R.drawable.ic_launcher_foreground,
-    var nameProfile: String = "Nome do perfil",
-    var timeProfile: String = "Agora",
+    var imageUser: Int = R.drawable.ic_launcher_foreground,
+    var nameUser: String = "Usuário",
+    var timeUser: String = "Agora",
     var imagePost: Int = R.drawable.ic_launcher_background,
-    var textPost: String = "Texto da postagem."
+    var textPost: String = "Texto da postagem.",
 )
