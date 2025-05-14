a5ecc96596f32f510fa06c55172a2a76435d1853
teacherlittletooth <94143910+teacherlittletooth@users.noreply.github.com>
2025-05-14 15:15:54 -0300

    Update PostList.kt

--- PostList.kt
+++ PostList.kt
@@ -1,52 +1,60 @@
-package br.qi.socialmediatarde
+package br.com.redeantisocialtarde
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import br.qi.socialmediatarde.model.PostData
-import br.qi.socialmediatarde.ui.theme.SocialMediaTardeTheme
+import br.com.redeantisocialtarde.model.PostData
+import br.com.redeantisocialtarde.ui.theme.RedeAntiSocialTardeTheme
 
 @Composable
 fun ListPost(
-    list: List<PostData> = List(10) { PostData() },
+    list: Array<PostData> = listagem,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(
-        state = LazyListState()
-    ) {
+    LazyColumn {
         items(list) {
-            Surface(
-                modifier = modifier.padding(8.dp)
-            ) {
-                PostPage(
-                    imageProfile = it.imageProfile,
-                    nameProfile = it.nameProfile,
-                    timeProfile = it.timeProfile,
-                    imagePost = it.imagePost,
-                    textPost = it.textPost,
-                )
-            }
+            PostPage(
+                imageUser = it.imageUser,
+                nameUser = it.nameUser,
+                timeUser = it.timeUser,
+                imagePost = it.imagePost,
+                textPost = it.textPost
+            )
         }
     }
 }
 
-///////////////////////////////////////////////
+//////////////////////////////////////////////
 @Preview(
     showBackground = true,
     heightDp = 400
 )
 @Composable
 fun ListPostPreview() {
-    SocialMediaTardeTheme {
+    RedeAntiSocialTardeTheme {
         ListPost()
     }
 }
+
+/////////////////////////////////////////////////////////
+
+val listagem = arrayOf<PostData>(
+    PostData(
+        imageUser = R.drawable.chaves_profile,
+        nameUser = "Chaves",
+        timeUser = "3 minutos atrás",
+        imagePost = R.drawable.chaves_post,
+        textPost = "Se você é jovem ainda..."
+    ),
+
+    PostData(
+        //imageUser = R.drawable.chaves_profile,
+        nameUser = "Kiko",
+        timeUser = "1 minuto atrás",
+        //imagePost = R.drawable.chaves_post,
+        textPost = "Você não vai com a minha cara?"
+    ),
+)
